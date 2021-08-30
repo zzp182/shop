91ded1048a62c33fff5549385ce99c23b98b7ae5
zzp812875759 <zhangzhengpeng2021@163.com>
2021-08-30 17:16:22 +0800

    商品分类表格建立

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -12,6 +12,16 @@
               <el-button type="primary">添加分类</el-button>
           </el-col>
       </el-row>
+      <!-- 表格 -->
+      <tree-table :data="catelist" :columns="columns" :selection-type="false" :expand-type="false" 
+        show-index index-text="#" border :show-row-hover="false">
+          <template slot="isok" slot-scope="scope">
+            <i class="el-icon-success" style="color:lightgreen" v-if="scope.row.cat_deleted === false"></i>
+            <i class="el-icon-error"   style="color:red" v-else></i>
+          </template>
+      </tree-table>
+      <!-- 分页区域 -->
+
     </el-card>
     </div>
 </template>
@@ -20,12 +30,45 @@
     export default {
         data(){
             return {
-                
+                //指定查询条件
+                queryInfo:{
+                  type: 3,
+                  pagenum: 1,
+                  pagesize: 5
+                },
+                //商品分类数据列表
+                catelist: [],
+                total:0,
+                columns:[
+                  {
+                    label:'分类名称',
+                    prop:'cat_name'
+                  },
+                  {
+                    label:'是否有效',
+                    // prop:'cat_deleted',
+                    //作用域插槽，模板列
+                    type:'template',
+                    //模板名称
+                    template:'isok'
+                  }
+                ]
             }
         },
-        created(){},
+        created(){
+          this.getCateList()
+        },
         methods:{
-
+          //获取商品分类数据
+          async getCateList(){
+            const {data: res } = await this.$http.get('categories' , { params: this.queryInfo})
+            if(res.meta.status !== 200){
+              return this.$message.error('获取商品分类失败！');
+            }
+            //数据赋值
+            this.catelist = res.data.result
+            this.total = res.data.total
+          }                    
         }
     }
 </script>
