ba14f7bc10266734dfd0d23c0d5865850472a479
zzp812875759 <zhangzhengpeng2021@163.com>
2021-10-15 17:21:30 +0800

    补全路由页面

---
+++ src/components/order/Order.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <!-- 面包屑导航区 -->
+    <el-breadcrumb el-breadseparator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-row :gutter="10">
+          <el-col >
+              <el-button type="primary">添加商品</el-button>
+          </el-col>
+      </el-row>
+    </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
---
+++ src/components/report/Report.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <!-- 面包屑导航区 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+        <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片视图 -->
+        <el-card>
+            <!-- 2.为Echarts准备一个Dom -->
+            <div id="main" style="width: 750px;height:400px;"></div>
+        </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
